1f1695edc071548ff36a9ba50db780f079f9c605
Kostamir <balciunask@gmail.com>
2020-11-09 17:42:37 +0200

    Gulp Part 2.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
+'use strict';
+
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const del = require('del');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const usemin = require('gulp-usemin');
+const rev = require('gulp-rev');
+const cleanCss = require('gulp-clean-css');
+const flatmap = require('gulp-flatmap');
+const htmlmin = require('gulp-htmlmin');
+
 
 function style() {
     return gulp.src('./css/*.scss')
@@ -22,6 +33,42 @@ function watch() {
 
 }
 
-exports.style = style;
-exports.watch = watch;
+function clean() {
+    return del('dist');
+}
+
+async function copyfonts() {
+    gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+    .pipe(gulp.dest('./dist/fonts'));
+}
+
+function imageminTask() {
+    return gulp.src('img/*.{png,jpg,gif}')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('dist/img'));
+}
+
+function useminTask() {
+    return gulp.src('./*.html')
+    .pipe(flatmap(function(stream, file){
+      return stream
+        .pipe(usemin({
+            css: [ rev() ],
+            html: [ function() { return htmlmin({ collapseWhitespace: true })} ],
+            js: [ uglify(), rev() ],
+            inlinejs: [ uglify() ],
+            inlinecss: [ cleanCss(), 'concat' ]
+        }))
+    }))
+    .pipe(gulp.dest('dist/'));
+}
+
+
+
+
+
 exports.default = watch;
+exports.clean = clean;
+exports.copyfonts = copyfonts;
+exports.imageminTask = imageminTask;
+exports.build = gulp.series(clean, gulp.parallel(copyfonts, imageminTask, useminTask));
